e4de60e67ff00b80efe583e455a5aaa199c26818
Laércio-Rodrigues <laerrcioo91@gmail.com>
2026-06-23 14:22:42 -0400

    Estrutura HTML do Projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 function App() {
-  
+
 
   return (
     <div>
-      <h1>Hello React</h1>
+      <input placeholder="O que tenha para fazer..." />
+      <button>Adicionar</button>
+
+      <ul>
+        <li>Levar o Nico para passear</li>
+        <li>Compar abacate</li>
+        <li>Terminar as aulas de React no DevClube</li>
+      </ul>
+
     </div>
   )
 }
